0d025c1815c3e4e3d06ba7394d3ff913a09eb120
Trevor Suarez <rican7@gmail.com>
2018-12-28 21:20:25 -0700

    Improving test data for the `transformer` package

--- transformer/transformer_test.go
+++ transformer/transformer_test.go
@@ -1,32 +1,38 @@
 package transformer
 
-import "testing"
-
-const snakeCaseJSON = `
-{
-    "title": "Example Title",
-    "description": "whatever",
-    "created_at": "2015-11-17T20:43:31.0463576-05:00",
-	"updated_at": "2018-12-24T13:21:15.7883416-07:00",
-    "is_active": true,
-    "image_url": "https://example.com/image.png",
-	"time_to_live": 600
-}
-`
-
-const camelCaseJSON = `
-{
-    "title": "Example Title",
-    "description": "whatever",
-    "createdAt": "2015-11-17T20:43:31.0463576-05:00",
-	"updatedAt": "2018-12-24T13:21:15.7883416-07:00",
-    "isActive": true,
-    "imageUrl": "https://example.com/image.png",
-	"timeToLive": 600
-}
-`
+import (
+	"testing"
+)
 
 func TestConventionalKeys(t *testing.T) {
+	const snakeCaseJSON = `
+	{
+		"title": "Example Title",
+		"description": "whatever",
+		"created_at": "2015-11-17T20:43:31.0463576-05:00",
+		"updated_at": "2018-12-24T13:21:15.7883416-07:00",
+		"is_active": true,
+		"image_url": "https://example.com/image.png",
+		"time_to_live": 600,
+		"$weird_key" : "with-colon-spacing-before-value",
+		"plan9_key" : "just an example with a numeral"
+	}
+	`
+
+	const camelCaseJSON = `
+	{
+		"title": "Example Title",
+		"description": "whatever",
+		"createdAt": "2015-11-17T20:43:31.0463576-05:00",
+		"updatedAt": "2018-12-24T13:21:15.7883416-07:00",
+		"isActive": true,
+		"imageUrl": "https://example.com/image.png",
+		"timeToLive": 600,
+		"$weirdKey" : "with-colon-spacing-before-value",
+		"plan9Key" : "just an example with a numeral"
+	}
+	`
+
 	// Compile-time functional-interface type check/enforcement "test"
 	var trans Transformer = ConventionalKeys()
 
