5ddc1c7f064ec0f909859f255e8110a6fdb1df2e
Trevor Suarez <rican7@gmail.com>
2019-01-03 15:40:19 -0700

    Improving the way that JSON key replacements are handled in the `transformer` package

--- transformer/transformer.go
+++ transformer/transformer.go
@@ -22,29 +22,41 @@ const (
 var (
 	keyMatchRegex             = regexp.MustCompile(`\"(.+?)\"\s*?:`)
 	camelCaseWordBarrierRegex = regexp.MustCompile(`(.)([A-Z])`)
-	snakeCaseWordBarrierRegex = regexp.MustCompile(`_(.)`)
+	snakeCaseWordBarrierRegex = regexp.MustCompile(`(?:[^_])_(.)`)
 )
 
 // ConventionalKeys TODO
 func ConventionalKeys() Transformer {
 	return func(data []byte, direction Direction) []byte {
-		return keyMatchRegex.ReplaceAllFunc(
+		return replaceKeys(
 			data,
-			func(match []byte) []byte {
+			func(key []byte) []byte {
 				if Unmarshal == direction {
-					return snakeCaseWordBarrierRegex.ReplaceAllFunc(match, func(match []byte) []byte {
-						// Remove the first byte (the underscore) from the match, and capitalize the rest
-						return bytes.ToUpper(match[1:])
+					return snakeCaseWordBarrierRegex.ReplaceAllFunc(key, func(match []byte) []byte {
+						// Remove the underscore from the match, and capitalize the rest
+						return append(match[0:1], bytes.ToUpper(match[2:])...)
 					})
 				}
 
 				return bytes.ToLower(
 					camelCaseWordBarrierRegex.ReplaceAll(
-						match,
+						key,
 						[]byte("${1}_${2}"),
 					),
 				)
 			},
 		)
 	}
+}
+
+func replaceKeys(data []byte, replaceFunc func(key []byte) []byte) []byte {
+	return keyMatchRegex.ReplaceAllFunc(
+		data,
+		func(match []byte) []byte {
+			key := replaceFunc(keyMatchRegex.FindSubmatch(match)[1])
+			indexes := keyMatchRegex.FindSubmatchIndex(match)
+
+			return append([]byte{match[indexes[0]]}, append(key, match[indexes[3]:indexes[1]]...)...)
+		},
+	)
 }
