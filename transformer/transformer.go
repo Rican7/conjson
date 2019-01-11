2b99e40199ff9879bbb62efc0fb3a7518cc350bf
Trevor Suarez <rican7@gmail.com>
2019-01-10 21:14:10 -0700

    Adding a new `CamelCaseKeys` transformer, and abstracting snake-case to camel-case transformations

--- transformer/transformer.go
+++ transformer/transformer.go
@@ -43,10 +43,7 @@ func ConventionalKeys() Transformer {
 			data,
 			func(key []byte) []byte {
 				if Unmarshal == direction {
-					return snakeCaseWordBarrierRegex.ReplaceAllFunc(key, func(match []byte) []byte {
-						// Remove the underscore from the match, and capitalize the rest
-						return append(match[0:1], bytes.ToUpper(match[2:])...)
-					})
+					return snakeCaseToCamelCaseWordBarrier(key)
 				}
 
 				return bytes.ToLower(
@@ -55,6 +52,23 @@ func ConventionalKeys() Transformer {
 						[]byte("${1}_${2}"),
 					),
 				)
+			},
+		)
+	}
+}
+
+// CamelCaseKeys TODO
+func CamelCaseKeys() Transformer {
+	return func(data []byte, direction Direction) []byte {
+		return replaceKeys(
+			data,
+			func(key []byte) []byte {
+				// Translate hyphenated keys to underscored ("snake_case")
+				key = bytes.Replace(key, []byte("-"), []byte("_"), -1)
+
+				key = snakeCaseToCamelCaseWordBarrier(key)
+
+				return append(bytes.ToLower(key[0:1]), key[1:]...)
 			},
 		)
 	}
@@ -92,4 +106,11 @@ func replaceKeys(data []byte, replaceFunc func(key []byte) []byte) []byte {
 			return append([]byte{match[indexes[0]]}, append(key, match[indexes[3]:indexes[1]]...)...)
 		},
 	)
+}
+
+func snakeCaseToCamelCaseWordBarrier(data []byte) []byte {
+	return snakeCaseWordBarrierRegex.ReplaceAllFunc(data, func(match []byte) []byte {
+		// Remove the underscore from the match, and capitalize the rest
+		return append(match[0:1], bytes.ToUpper(match[2:])...)
+	})
 }
